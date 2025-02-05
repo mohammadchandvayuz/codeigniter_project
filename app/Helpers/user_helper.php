2d07096416c1c2433d5e983875f672ebb8fc51f9
mohammadchandvayuz <mohammadchand.vayuz@gmail.com>
2025-02-05 16:40:54 +0530

    Added static data changes in user helper

--- app/Helpers/user_helper.php
+++ app/Helpers/user_helper.php
@@ -9,3 +9,32 @@ if (!function_exists('get_users')) {
         return $userModel->getUsers();
     }
 }
+
+if (!function_exists('get_static_user_data')) {
+    function get_static_user_data()
+    {
+        return [
+            [
+                'id' => 1,
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'email' => 'john.doe@example.com',
+                'role' => 'admin'
+            ],
+            [
+                'id' => 2,
+                'first_name' => 'Jane',
+                'last_name' => 'Smith',
+                'email' => 'jane.smith@example.com',
+                'role' => 'customer'
+            ],
+            [
+                'id' => 3,
+                'first_name' => 'Alice',
+                'last_name' => 'Brown',
+                'email' => 'alice.brown@example.com',
+                'role' => 'customer'
+            ]
+        ];
+    }
+}
