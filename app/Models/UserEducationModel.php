ce32941cdcc55e7cbedf809727d57194bdd7e6e6
mohammadchandvayuz <mohammadchand.vayuz@gmail.com>
2025-02-05 16:04:13 +0530

    Created new function in User Education Model

--- app/Models/UserEducationModel.php
+++ app/Models/UserEducationModel.php
@@ -7,4 +7,8 @@ class UserEducationModel extends Model {
     protected $table = 'user_education';
     protected $primaryKey = 'id';
     protected $allowedFields = ['user_id', 'degree', 'institution', 'passing_year'];
+
+    public function getEducationByUserId($userId) {
+        return $this->where('user_id', $userId)->findAll();
+    }
 }
