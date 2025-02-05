014af5790bfbcfde0b7131b91dac721d732c08b2
mohammadchandvayuz <mohammadchand.vayuz@gmail.com>
2025-02-05 16:07:14 +0530

    Created new function to get static data in User Education Model

--- app/Models/UserEducationModel.php
+++ app/Models/UserEducationModel.php
@@ -7,4 +7,31 @@ class UserEducationModel extends Model {
     protected $table = 'user_education';
     protected $primaryKey = 'id';
     protected $allowedFields = ['user_id', 'degree', 'institution', 'passing_year'];
+
+    // Function to return static user education data
+    public function getStaticEducationData() {
+        return [
+            [
+                'id' => 1,
+                'user_id' => 1,
+                'degree' => 'B.Sc Computer Science',
+                'institution' => 'XYZ University',
+                'passing_year' => 2020
+            ],
+            [
+                'id' => 2,
+                'user_id' => 1,
+                'degree' => 'M.Sc Computer Science',
+                'institution' => 'ABC University',
+                'passing_year' => 2022
+            ],
+            [
+                'id' => 3,
+                'user_id' => 2,
+                'degree' => 'B.Tech Information Technology',
+                'institution' => 'LMN College',
+                'passing_year' => 2019
+            ]
+        ];
+    }
 }
