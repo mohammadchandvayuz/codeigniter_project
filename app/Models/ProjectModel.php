6db628274d0a3296bb99bbb8b9f8d7a9f1fd61d3
mohammadchandvayuz <mohammadchand.vayuz@gmail.com>
2025-02-05 16:11:49 +0530

    Created new function to get static data in Project Model

--- app/Models/ProjectModel.php
+++ app/Models/ProjectModel.php
@@ -10,5 +10,25 @@ class ProjectModel extends Model
     protected $table    = 'projects'; 
     protected $primaryKey = 'id';
     protected $allowedFields = ['name', 'description'];
+
+    public function getStaticProjectData() {
+        return [
+            [
+                'id' => 1,
+                'name' => 'Project Alpha',
+                'description' => 'A web-based management system for businesses.'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Project Beta',
+                'description' => 'A mobile application for online shopping.'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Project Gamma',
+                'description' => 'An AI-powered chatbot for customer service.'
+            ]
+        ];
+    }
 }
 
