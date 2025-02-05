e2791a26f60f1247f40a52709075f83dfa628b2c
mohammadchandvayuz <mohammadchand.vayuz@gmail.com>
2025-02-05 14:05:59 +0530

    Home Controller added comments

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    // customer message after loggin
     public function index(): string
     {
         return view('welcome_message');
